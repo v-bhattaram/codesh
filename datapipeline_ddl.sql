9cacf243cb5e65e85727c72442a18680bd08983d
v-bhattaram <136536325+v-bhattaram@users.noreply.github.com>
2025-09-17 01:08:47 -0400

    Add files via upload

---
+++ datapipeline_ddl.sql
@@ -0,0 +1,47 @@
+DDL
+    datapipeline_manager
+        datapipeline_id
+        datapipeline_name
+        handler_path
+        handler_script_name
+        notes
+        run_frequency
+        parms_template_id
+        status
+        created_by
+        created_date
+        updated_by
+        updated_date
+
+    datapipeline_run
+        datapipeline_run_id
+        rundatetime
+        message
+        status  
+        datapipeline_id
+        created_by
+        created_date
+        updated_by
+        updated_date
+
+    datapipeline_runlog
+        datapipeline_runlog_id
+        message
+        datapipeline_id    
+        created_by
+        created_date
+        updated_by
+        updated_date
+
+    datapipeline_errorlog
+        datapipeline_errorlog_id
+        message
+        datapipeline_id
+        created_by
+        created_date
+        updated_by
+        updated_date
+    
+
+
+Code Generator
